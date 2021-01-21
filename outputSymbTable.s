8cb6f785771c66bf16c388bae6a5792ee55d084a
Jyoti Suryawanshi <52448855+jyotisuryawanshi2527@users.noreply.github.com>
2021-01-21 22:05:39 +0530

    Output-Symbol-Table

---
+++ outputSymbTable.s
@@ -0,0 +1,15 @@
+    LINE    NAME    TYPE    ADDR     D/U    SIZE
+       2    var1     var       0       D      15
+       3    var2     var      15       D      17
+       4    var3     var      32       D       1
+       5    var4     var      33       D       0
+       6    var5     var      33       D       4
+       7    var6     var      37       D       8
+       8    var7     var      45       D      12
+       9    var8     var      57       D      12
+      12    main   label       0       D       0
+      14      l1   label       0       D       0
+      17      l5   label     117       D       0
+      21      l2   label      27       D       0
+      29      l3   label      59       D       0
+      36      l4   label      88       D       0
